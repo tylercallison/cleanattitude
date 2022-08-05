01fb16d0b819889f8eccc62064fe565d9c70d8d5
tylercallison <tcsoccer2000@gmail.com>
2022-08-05 13:23:40 -0700

    fix: add other pages

---
+++ pages/about.tsx
@@ -0,0 +1,27 @@
+import { Fragment, ReactElement } from "react";
+import { Layout } from "../components/layout";
+import Meta from "../components/meta";
+import Nav from "../components/nav";
+import Footer from "../components/footer";
+
+const About = () => {
+  return <Fragment></Fragment>;
+};
+
+About.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>
+      <Meta
+        title="Home"
+        description="Clean Attitude - Carpet Cleaning and More"
+        robots={true}
+        keywords="clean, attitude, san, diego, carpet, cleaning, more, cheap, quality, home, house"
+      />
+      <Nav />
+      <main>{page}</main>
+      <Footer />
+    </Layout>
+  );
+};
+
+export default About;
