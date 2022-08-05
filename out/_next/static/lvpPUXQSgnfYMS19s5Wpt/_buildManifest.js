4f3b0a940a08c3f69b46cd2f8252efe10f9c1220
tylercallison <tcsoccer2000@gmail.com>
2022-08-05 08:18:17 -0700

    init

---
+++ out/_next/static/lvpPUXQSgnfYMS19s5Wpt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-37f7ca54cbe71f58.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
